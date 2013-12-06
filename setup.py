cc5c010e790446301208dfcc2e1389b11b6ffa16
Joe Zuntz <joezuntz@googlemail.com>
2013-12-06 11:16:32 +0000

    manually used changes from Steven Murray to improve installation - many thanks!

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from numpy.distutils.core import setup, Extension
 from numpy import get_include
+import numpy.version
+from distutils.version import StrictVersion
+
 import generatePyCamb
 import os.path
 import sys
@@ -9,8 +12,6 @@ if '--nonstop' in sys.argv:
 else:
   from numpy import f2py
 
-# Get CAMB from http://camb.info, untar and copy *.[fF]90 to src/
-# this is done by the script extract_camb.sh
 
 cambsources = ['camb/%s' % f for f in [
     'constants.f90',
@@ -32,7 +33,7 @@ cambsources = ['camb/%s' % f for f in [
 
 for f in cambsources:
   if not os.path.exists(f):
-    raise Exception("At least one of CAMB code file: '%s' is not found. Download and extract to camb/" % f)
+    raise Exception("At least one of CAMB code file: '%s' is not found. Download and extract to camb/.  You can use teh extract_camb.sh script to help" % f)
 
 try: os.mkdir('src')
 except: pass
@@ -41,25 +42,34 @@ generatePyCamb.main()
 f2py.run_main(['-m', '_pycamb', '-h', '--overwrite-signature', 'src/py_camb_wrap.pyf', 
          'src/py_camb_wrap.f90', 'skip:', 'makeparameters', ':'])
 
-setup(name="pycamb", version="0.1",
+# Newer versions of f2py (from numpy >= 1.6.2) use specific f90 compile args
+if StrictVersion(numpy.version.version) > StrictVersion('1.6.1'):
+    pycamb_ext = Extension("pycamb._pycamb",
+                           ['src/py_camb_wrap.pyf'] + cambsources + ['src/py_camb_wrap.f90'],
+                           extra_f90_compile_args=['-O0', '-g', '-Dintp=npy_intp', '-fopenmp'],
+                           libraries=['gomp'],
+                           include_dirs=[get_include()],
+                           )
+else:
+    Extension("pycamb._pycamb",
+             ['src/py_camb_wrap.pyf'] + cambsources + ['src/py_camb_wrap.f90'],
+             extra_compile_args=['-O0', '-g', '-Dintp=npy_intp'],
+             include_dirs=[get_include()],
+             )
+
+# Perform setup
+setup(name="pycamb", version="0.3",
       author="Joe Zuntz",
-      author_email="jaz@astro.ox.ac.uk",
+      author_email="joezuntz@googlemail.com",
       description="python binding of camb, you need sign agreement and obtain camb source code to build this. Thus we can not GPL this code.",
-      url="http://github.com/rainwoodman/#",
-      download_url="http://web.phys.cmu.edu/~yfeng1/#",
+      url="http://github.com/joezuntz/pycamb",
       zip_safe=False,
       install_requires=['numpy'],
       requires=['numpy'],
-      packages = [ 'pycamb' ],
-      package_dir = {'pycamb': 'src'},
-      data_files = [('pycamb/camb', ['camb/HighLExtrapTemplate_lenspotentialCls.dat'])],
-      scripts = [],
-      ext_modules = [
-        Extension("pycamb._pycamb", 
-             ['src/py_camb_wrap.pyf'] + cambsources +['src/py_camb_wrap.f90'],
-             extra_compile_args=['-O0', '-g', '-Dintp=npy_intp'],
-#             libraries = {'noexit': ['src/noexit.c']},
-             include_dirs=[get_include()],
-        )]
+      packages=[ 'pycamb' ],
+      package_dir={'pycamb': 'src'},
+      data_files=[('pycamb/camb', ['camb/HighLExtrapTemplate_lenspotentialCls.dat'])],
+      scripts=[],
+      ext_modules=[pycamb_ext]
     )
 
